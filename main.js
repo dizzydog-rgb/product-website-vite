76e3b0089f99af22253a94634da59950ffcd33d4
Jackie_Chen <fox29582@yahoo.com.tw>
2024-05-20 16:26:13 +0800

    update header

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import './assets/scss/all.scss';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/scss/bootstrap.scss';
+import './_all.js'
+
 console.log("Hello world!");
