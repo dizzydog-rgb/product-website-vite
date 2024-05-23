e8fcf9d8e158e713a53b9924e952df9fbba6eb20
Jackie_Chen <fox29582@yahoo.com.tw>
2024-05-23 20:21:02 +0800

    index complete

--- _all.js
+++ _all.js
@@ -1,2 +1,16 @@
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+const hambugerList = document.querySelector('.hambuger-list');
 
+menuIcon.addEventListener('click', () => {
+    hambugerList.classList.toggle('hambuger-list-show');
+    menuIcon.classList.toggle('unshow');
+    closeIcon.classList.toggle('show');
+});
+
+closeIcon.addEventListener('click', () => {
+        hambugerList.classList.toggle('hambuger-list-show');
+        closeIcon.classList.toggle('show');
+        menuIcon.classList.toggle('unshow');
+    })
 
