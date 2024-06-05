90758f83d6d32f1d30ca19906074b5431e5e7497
Jackie_Chen <fox29582@yahoo.com.tw>
2024-06-05 15:45:55 +0800

    blog page & swiper update

--- _all.js
+++ _all.js
@@ -9,8 +9,35 @@ menuIcon.addEventListener('click', () => {
 });
 
 closeIcon.addEventListener('click', () => {
-        hambugerList.classList.toggle('hambuger-list-show');
-        closeIcon.classList.toggle('show');
-        menuIcon.classList.toggle('unshow');
-    })
+    hambugerList.classList.toggle('hambuger-list-show');
+    closeIcon.classList.toggle('show');
+    menuIcon.classList.toggle('unshow');
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
 
